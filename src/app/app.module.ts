4db1fd3688cfd3ee56ba09db188969313d707a98
thatssushmit <thatsushmit@gmail.com>
2022-03-01 15:56:25 +0530

    added initial component for heade and side panel

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
+import { SidePanelComponent } from './side-panel/side-panel.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent,
+    SidePanelComponent
   ],
   imports: [
     BrowserModule,
